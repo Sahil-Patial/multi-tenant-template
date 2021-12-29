7672e9a4bdeb1b7f8d3949bf4a87c5a98f666129
sharmanikhil1100 <sharmanikhil1100@gmail.com>
2021-12-29 11:49:08 +0530

    deleted duplicate files after changing package name

--- src/main/java/com/multiTenantDbImpl2/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.multiTenantDbImpl2.repository;
-
-import com.multiTenantDbImpl2.repository.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
