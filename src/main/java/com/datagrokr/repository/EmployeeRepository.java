d225d0e210365d4ee23969cc092281f0819ac1d7
Sahil Patial <sahil.patial@datagrokr.com>
2022-02-02 06:29:00 +0000

    added findbyname method

--- src/main/java/com/datagrokr/repository/EmployeeRepository.java
+++ src/main/java/com/datagrokr/repository/EmployeeRepository.java
@@ -11,4 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+  public Employee findByEmpName(String empName);
 }
